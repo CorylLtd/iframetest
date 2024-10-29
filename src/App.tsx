66776333b928847ba1ce92b2bd4a3c6c01d5f9e3
Martin Randall <martin@coryl.com>
2024-10-29 22:50:25 +0000

    Added iframe detection

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,52 @@ import {
   useMsal,
 } from '@azure/msal-react';
 import { loginRequest } from './auth/authConfig';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [isInIframe, setIsInIframe] = useState(false);
+
+  useEffect(() => {
+    setIsInIframe(window.self !== window.top);
+  }, []);
+
   const { instance } = useMsal();
   const activeAccount = instance.getActiveAccount();
 
   const handleRedirect = () => {
-    instance
-      .loginRedirect({
-        ...loginRequest,
-        prompt: 'create',
-      })
-      .catch((error) => console.log(error));
+    if (isInIframe) {
+      instance
+        .loginPopup({
+          ...loginRequest,
+          prompt: 'create',
+        })
+        .catch((error) => console.log(error));
+    } else {
+      instance
+        .loginRedirect({
+          ...loginRequest,
+          prompt: 'create',
+        })
+        .catch((error) => console.log(error));
+    }
   };
 
   const handleLogout = () => {
-    instance.logoutRedirect().catch((error) => console.log(error));
+    if (isInIframe) {
+      instance.logoutPopup().catch((error) => console.log(error));
+    } else {
+      instance.logoutRedirect().catch((error) => console.log(error));
+    }
   };
   return (
     <div className='App'>
+      <div>
+        {isInIframe ? (
+          <p>The app is running inside an iframe.</p>
+        ) : (
+          <p>The app is running outside an iframe.</p>
+        )}
+      </div>
       <AuthenticatedTemplate>
         {activeAccount ? (
           <p>you are authenticated, {activeAccount.name}</p>
